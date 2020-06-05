efac4cc3bdbdeebc45aed875891328ddbfd59932
jonowu <53251695+jonowu@users.noreply.github.com>
2020-06-05 23:54:22 +1000

    feat: add head element

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
+
 import GlobalStyle from '../components/global-style';
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <title>Emojinary</title>
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
