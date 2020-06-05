bdcf095bfe86af4af6e6fad978460215c7e6dd33
jonowu <53251695+jonowu@users.noreply.github.com>
2020-06-05 15:38:48 +1000

    feat: add background color

--- components/global-style/index.js
+++ components/global-style/index.js
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   html,
   body {
+    margin: 0;
+    padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
       Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
--- components/layout/index.js
+++ components/layout/index.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  overflow: hidden;
+  position: relative;
+  background: #fffff3;
+  padding-bottom: 74px;
+`;
 
 const Layout = ({ children }) => {
   return <Wrapper>{children}</Wrapper>;
