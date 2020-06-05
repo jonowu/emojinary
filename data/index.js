87f57e97b5c7e539a02d8a43d38e66e663a92372
jonowu <53251695+jonowu@users.noreply.github.com>
2020-06-05 18:08:57 +1000

    feat: add data, player input, answer checking, and scores

---
+++ data/index.js
@@ -0,0 +1,7 @@
+const movies = [
+  { name: 'theIncredibles', emoji: '👩‍👩‍👧‍👦🦸‍♂️', answers: ['the incredibles', 'incredibles'] },
+  { name: 'findingNemo', emoji: '🐟', answers: ['dory', 'finding nemo'] },
+  { name: 'toyStory', emoji: '🤠👨‍🚀', answers: ['toy story'] },
+];
+
+export { movies };
--- pages/play.js
+++ pages/play.js
@@ -1,24 +1,77 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Flex, Box } from 'reflexbox';
+import _ from 'lodash';
 
 import Layout from '../components/layout';
 import Button from '../components/button';
+import { Col, Row } from '../components/grid';
+import { movies } from '../data';
 
-const Heading = styled.h1`
+const Heading = styled.h2`
   text-align: center;
   font-size: 2rem;
 `;
 
+const StyledInput = styled.input`
+  padding: 0 1rem;
+  height: 50px;
+  width: 100%;
+  border-style: solid;
+  font-weight: bold;
+`;
+
+const Emoji = styled.div`
+  font-size: 4rem;
+`;
+
 const Play = () => {
+  const [movie, setMovie] = useState('');
+  const [points, setPoints] = useState(0);
+  const [playerInput, setPlayerInput] = useState('');
+
+  useEffect(() => {
+    const randomMovie = _.shuffle(movies)[0];
+    setMovie(randomMovie);
+  }, [points]);
+
+  const checkAnswer = () => {
+    let correct = false;
+    _.each(movie.answers, (answer) => {
+      if (playerInput === answer) {
+        correct = true;
+        setPoints(points + 1);
+      }
+    });
+    correct ? console.log('correct') : console.log('incorrect');
+  };
+
   return (
     <Layout>
-      <Heading>Guess the Movie!</Heading>
-
-      <Flex justifyContent="center" mt="3rem">
+      <Row justifyContent="center">
+        <Heading>Guess the Movie!</Heading>
+      </Row>
+      <Row justifyContent="center" mb="2rem">
+        <div>Points: {points}</div>
+      </Row>
+      <Row justifyContent="center" mb="2rem">
+        <Emoji>{movie.emoji}</Emoji>
+      </Row>
+      <Row justifyContent="center" mb="2rem" mx="5rem">
+        <StyledInput
+          type="text"
+          placeholder="Please enter your answer"
+          value={playerInput}
+          onChange={(e) => setPlayerInput(e.target.value)}
+        />
+      </Row>
+      <Row justifyContent="center">
         <Box>
-          <Button type={'button'}>Submit</Button>
+          <Button type={'button'} onClick={checkAnswer}>
+            Submit
+          </Button>
         </Box>
-      </Flex>
+      </Row>
     </Layout>
   );
 };
