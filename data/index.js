73af2853c02a55497115437ab32484e106b1601f
jonowu <53251695+jonowu@users.noreply.github.com>
2020-07-07 18:46:40 +1000

    feat: add more movies

--- data/index.js
+++ data/index.js
@@ -1,11 +1,34 @@
 const movies = [
-  { name: 'theIncredibles', emoji: '👩‍👩‍👧‍👦🦸‍♂️', answers: ['the incredibles', 'incredibles'], hint: '"No capes!"' },
-  { name: 'toyStory', emoji: '🤠👨‍🚀', answers: ['toy story'], hint: '"There’s a snake in my boot!"' },
-  { name: 'jaws', emoji: '⛵️🦈💀', answers: ['jaws'], hint: '"We’re going to need a bigger boat"' },
-  { name: 'kungFuPanda', emoji: '🐼👊', answers: ['kung fu panda'], hint: 'Jack Black plays a fighting panda.' },
-  { name: 'titanic', emoji: '🚢', answers: ['titanic'], hint: 'My Heart Will Go On.' },
-  { name: 'theLionKing', emoji: '🦁👑', answers: ['lion king', 'the lion king'], hint: '"Hakuna Matata."' },
-  { name: 'it', emoji: '🤡🎈', answers: ['it'], hint: '"You’ll float too."' },
+  { name: 'theIncredibles', emoji: '👨‍👩‍👧‍👦🦸‍♂️', answers: ['the incredibles', 'incredibles'], hint: 'No capes!' },
+  { name: 'toyStory', emoji: '🤠👨‍🚀', answers: ['toy story'] },
+  { name: 'jaws', emoji: '⛵️🦈💀', answers: ['jaws'] },
+  { name: 'kungFuPanda', emoji: '🐼👊', answers: ['kung fu panda'] },
+  { name: 'titanic', emoji: '🗻🚢', answers: ['titanic'] },
+  { name: 'theLionKing', emoji: '🦁👑', answers: ['lion king', 'the lion king'] },
+  { name: 'it', emoji: '🤡🎈', answers: ['it'] },
+  {
+    name: 'madMaxFuryRoad',
+    emoji: '🤬🧔🏻:🔥🚗',
+    answers: ['mad max fury road', 'fury road', 'mad max', 'mad max - fury road', 'mad max: fury road'],
+  },
+  { name: 'johnWick', emoji: '🐶🔫 😠', answers: ['john wick'], hint: '✏️' },
+  { name: 'zootopia', emoji: '🦊🐰🚔', answers: ['zootopia'] },
+  { name: 'aQuietPlace', emoji: '👨‍👩‍👧‍👦🦸‍🤫👽', answers: ['quiet place', 'a quiet place'], hint: 'Shhhh' },
+  { name: 'charlottesWeb', emoji: '🐖🕷🕸', answers: ["charlotte's web", 'charlottes web'] },
+  { name: 'wizardOfOz', emoji: '🦁🤖🌾👧👠', answers: ['wizard of oz', 'the wizard of oz'] },
+  { name: 'whiplash', emoji: '🥁😡', answers: ['whiplash'], hint: 'Not quite my tempo.' },
+  { name: 'backToTheFuture', emoji: '🙍‍♂️👴⏰🚙', answers: ['back to the future'] },
+  {
+    name: 'ferrisBuellersDayOff',
+    emoji: '🤒 + 🤥 = 🤷‍♂️🚘',
+    answers: [
+      'ferris buellers day off',
+      'ferris beullers day off',
+      "ferris bueller's day off",
+      "ferris beuller's day off",
+    ],
+    hint: 'Life moves pretty fast.',
+  },
 ];
 
 export { movies };
